afb1359fd80aabda6b6939eb9d704f4e6f35a0d6
Julie Poulain <juliempoulain@gmail.com>
2024-04-10 10:55:31 -0700

    display plants from db

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plants) => {
+        setPlants(plants)
+        console.log(plants);
+      });
+  }, []);
+
+  const plantCards = plants.map((plant) => (
+    <PlantCard
+      key={plant.id}
+      image={plant.image}
+      name={plant.name}
+      price={plant.price}
+    />
+  ))
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantCards}</ul>
   );
 }
 
